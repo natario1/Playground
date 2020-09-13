c7c593e21e6cda115a9486a11d8780c56d483350
Mattia Iavarone <mat.iavarone@gmail.com>
2020-09-13 12:42:12 +0200

    Simplify build.gradle

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -25,21 +25,16 @@ fun KotlinMultiplatformExtension.newSourceSet(name: String, parent: KotlinSource
 
 fun KotlinMultiplatformExtension.androidNative(name: String = "androidNative", configure: KotlinNativeTarget.() -> Unit) {
     val androidNativeMain = newSourceSet("${name}Main", sourceSets["commonMain"])
-    val androidNativeTest = newSourceSet("${name}Test", sourceSets["commonTest"])
     val androidNative32BitMain = newSourceSet("${name}32BitMain", androidNativeMain)
-    val androidNative32BitTest = newSourceSet("${name}32BitTest", androidNativeTest)
     val androidNative64BitMain = newSourceSet("${name}64BitMain", androidNativeMain)
-    val androidNative64BitTest = newSourceSet("${name}64BitTest", androidNativeTest)
     val targets32 = listOf(androidNativeX86(), androidNativeArm32())
     val targets64 = listOf(androidNativeX64(), androidNativeArm64())
     targets32.forEach {
         newSourceSet(it.compilations["main"].defaultSourceSet.name, androidNative32BitMain)
-        newSourceSet(it.compilations["test"].defaultSourceSet.name, androidNative32BitTest)
         it.configure()
     }
     targets64.forEach {
         newSourceSet(it.compilations["main"].defaultSourceSet.name, androidNative64BitMain)
-        newSourceSet(it.compilations["test"].defaultSourceSet.name, androidNative64BitTest)
         it.configure()
     }
 }
