60e344df528bd225adb234f145895dd623952f2e
Mattia Iavarone <mat.iavarone@gmail.com>
2020-09-15 11:57:47 +0200

    Bug

--- library/src/androidNativeMain/kotlin/AndroidNative.kt
+++ library/src/androidNativeMain/kotlin/AndroidNative.kt
@@ -1,1 +1,9 @@
-fun androidNative() = Unit
+interface TestInterface {
+    fun doSomething(): Int
+}
+
+fun TestInterface(something: Int): TestInterface = object : TestInterface {
+    override fun doSomething(): Int {
+        return something
+    }
+}
