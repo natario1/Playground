0f1285209787b70bf36b5a1220eee66fa8c13823
Mattia Iavarone <mat.iavarone@gmail.com>
2020-09-21 14:17:21 +0200

    Cleanup

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -12,22 +12,6 @@ fun KotlinMultiplatformExtension.newSourceSet(name: String, parent: KotlinSource
     }
 }
 
-fun KotlinMultiplatformExtension.androidNativeOld(name: String = "androidNative", configure: KotlinNativeTarget.() -> Unit) {
-    val androidNativeMain = newSourceSet("${name}Main", sourceSets["commonMain"])
-    val androidNative32BitMain = newSourceSet("${name}32BitMain", androidNativeMain)
-    val androidNative64BitMain = newSourceSet("${name}64BitMain", androidNativeMain)
-    val targets32 = listOf(androidNativeX86(), androidNativeArm32())
-    val targets64 = listOf(androidNativeX64(), androidNativeArm64())
-    targets32.forEach {
-        newSourceSet(it.compilations["main"].defaultSourceSet.name, androidNative32BitMain)
-        it.configure()
-    }
-    targets64.forEach {
-        newSourceSet(it.compilations["main"].defaultSourceSet.name, androidNative64BitMain)
-        it.configure()
-    }
-}
-
 fun KotlinMultiplatformExtension.androidNative(name: String = "androidNative", configure: KotlinNativeTarget.() -> Unit) {
     val androidNativeMain = newSourceSet("${name}Main", sourceSets["commonMain"])
     val targets = listOf(androidNativeX86(), androidNativeArm32(), androidNativeX64(), androidNativeArm64())
